3393bd8ceab269fabf3c024be7b2ffef4aef7896
Gabriel Cabrejas <gabriel@cabrejas.net>
2023-10-31 16:49:45 +0100

    test

--- frontend/components/__tests__/Todo.test.js
+++ frontend/components/__tests__/Todo.test.js
@@ -27,7 +27,7 @@ afterEach(() => {
 })
 
 describe('Todo component', () => {
-  test('can grab todos using Axios', async () => {
+  test('can grab todos using fetch', async () => {
     await waitFor(() => {
       expect(screen.getByText(/laundry/, queryOptions)).toBeVisible()
       expect(screen.getByText(/dishes/, queryOptions)).toBeVisible()
@@ -35,7 +35,7 @@ describe('Todo component', () => {
     }, waitForOptions)
   })
 
-  test('Can mark todo complete using Axios', async () => {
+  test('Can mark todo complete using fetch', async () => {
     const user = userEvent.setup()
     await user.click(await screen.findByText(/laundry/, queryOptions, waitForOptions))
     expect(await screen.findByText('laundry ✔️', queryOptions, waitForOptions)).toBeVisible()
